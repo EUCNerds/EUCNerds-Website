cea854a9f0f8d79ae749fc11eedf8e8c7dda93cc
EUCNerds <nick@eucnerds.com>
2025-07-20 21:55:36 -0400

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,47 +1,42 @@
-// Source and credits to: https://github.com/pew/cloudflare-pages-social-preview
 class ElementHandler {
   constructor(ogtag) {
-    this.ogtag = ogtag
+    this.ogtag = ogtag;
   }
   element(element) {
-    element.append(this.ogtag, { html: true })
+    element.append(this.ogtag, { html: true });
   }
 }
 
 export async function onRequest(context) {
-  const { request, next } = context
-  const res = await next()
-  const { searchParams, pathname } = new URL(request.url)
+  const { request, next } = context;
+  const res = await next();
+  const { searchParams, pathname } = new URL(request.url);
 
   if (!(pathname === '/index.html' || pathname === '/')) {
-    return res
+    return res;
   }
 
-  // Metatags Variables (edit these!)
-  const metatitle = "Cloudflare Pages Template"
-  const metadescription = "Get started with Cloudflare Pages by deploying this pretty simple template on your own account, and start playing around."
+  // ✅ Use your real metadata here
+  const metatitle = "EUC Nerds – End User Computing Experts";
+  const metadescription = "Expert End User Computing solutions for Citrix, Azure, Microsoft 365, and more.";
+  const ogImage = "https://eucnerds.com/img/eucnerds_blue.png";
+  const pageUrl = "https://eucnerds.com/";
 
-  let name = searchParams.get('myQuery')
-  let ogtag
-
-  // these are the metatags we want to inject into the site
-  ogtag = `
+  const ogtag = `
     <meta property="og:title" content="${metatitle}" />
     <meta property="og:description" content="${metadescription}" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="${request.url}img/preview.png?${name ? 'myQuery=' + name : 'default'}" />
-
+    <meta property="og:url" content="${pageUrl}" />
+    <meta property="og:image" content="${ogImage}" />
     <meta property="og:image:height" content="630" />
     <meta property="og:image:width" content="1200" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="${metatitle}" />
     <meta name="twitter:description" content="${metadescription}" />
+    <meta name="description" content="${metadescription}" />
+  `;
 
-    <meta name="description" content="and even more stuff about my page" />
-  `
-
-  return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
+  return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res);
 }
